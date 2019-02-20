f9c0d4abdedf6d263923dd508f4ba68d58fd2b84
tom1mol <tomasomaolmhuaidh@gmail.com>
2019-02-20 14:43:04 +0000

    Update

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -17,10 +17,8 @@ connection = pymysql.connect(host='localhost',
 try:
     #run a query
     with connection.cursor() as cursor:
-        rows = [("Bob", 21, "1990-02-06 23:04:56"),      #list seperate by commas..whole thing into square brackets rows plural
-                ("Jim", 56, "1955-05-09 13:12:45"),
-                ("Fred", 100, "1911-09-12 01:01:01")]
-        cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", rows)    #rows plural. executemany                     
+        
+        cursor.execute("UPDATE Friends SET age = 22 where name = 'Bob';")                     
         connection.commit()
 finally:
     #Close the connection, regardless of whether the above was successful
